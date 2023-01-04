80f9ac4f6207f358be2d7b8bbafcaef0505815fa
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-01-04 14:04:39 -0500

    check the osmosis testnet aUSDC balance
    it's zero

--- 0_test.ts
+++ 0_test.ts
@@ -1,8 +1,5 @@
 import { AxelarAssetTransfer, AxelarQueryAPI, Environment, CHAINS, AxelarGMPRecoveryAPI, GMPStatusResponse } from "@axelar-network/axelarjs-sdk";
-
 import { SigningCosmWasmClient, Secp256k1HdWallet, GasPrice } from "cosmwasm";
-
-
 
 const osmoRpc = "https://rpc-test.osmosis.zone";
 
@@ -18,39 +15,25 @@ const recoveryApi = new AxelarGMPRecoveryAPI({
     environment: Environment.TESTNET,
 });
 
-const mnemonic = "matrix asthma dutch profit young depart toast swim voice code century announce";
+const mnemonic = "volcano sure feed scrub fence close fetch link race cart rude cost";
 
-
-async function setupClient(mnemonic: string, rpc: string, gas: string | undefined): Promise<SigningCosmWasmClient> {
-    if (gas === undefined) {
-        let wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'osmo' });
-        let client = await SigningCosmWasmClient.connectWithSigner(rpc, wallet);
-
-        return client;
-    } else {
-        let gas_price = GasPrice.fromString(gas);
-        let wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'osmo' });
-        let client = await SigningCosmWasmClient.connectWithSigner(rpc, wallet, { gasPrice: gas_price });
-        return client;
-    }
+async function setupClient(mnemonic: string, rpc: string): Promise<SigningCosmWasmClient> {
+  let wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'osmo' });
+  return await SigningCosmWasmClient.connectWithSigner(rpc, wallet);
 }
 
 async function getAddress(mnemonic: string, prefix: string = 'osmo') {
-    let wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix });
-    let accounts = await wallet.getAccounts();
-    return accounts[0].address;
+  let wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix });
+  let accounts = await wallet.getAccounts();
+  return accounts[0].address;
 }
 
 
 describe("Axelar-js tests", () => {
-    it("Generate Wallet", async () => {
-        let wallet = await Secp256k1HdWallet.generate(12);
-        console.log(wallet.mnemonic);
-    });
-
-    xit("gets deposit address", async () => {
-        let address = await getAddress(mnemonic);
-
-    });
-
+  it("gets deposit address", async () => {
+    const address = await getAddress(mnemonic);
+    let client = await setupClient(mnemonic, osmoRpc);
+    let balance = await client.getBalance(address, "uausdc");
+    console.log({ address, balance } );
+  });
 });
